5a5bdd84464df234da045913394aed4c3508ac6f
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-03-16 15:26:40 -0300

    feat: get country information

---
+++ frontEnd/src/hooks/apiCovidKidopi/useGetCountryInfo.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import axios from 'axios'
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+
+export interface IInfo {
+  Pais: string
+  ProvinciaEstado: string
+  Mortos: number
+  Confirmados: number
+}
+
+export function useGetCountryInfo() {
+  const [info, setInfo] = useState<IInfo[] | null>(null)
+  const [date, setDate] = useState('')
+  const [country, setCountry] = useState('')
+  const [confirmed, setConfirmed] = useState<number | null>(null)
+  const [dead, setDead] = useState<number | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  const fetch = (country: string) => {
+    if (loading) return
+    setLoading(true)
+
+    const now = dayjs().format('DD/MM/YYYY HH:mm:ss')
+    setDate(now)
+
+    const URL = `https://dev.kidopilabs.com.br/exercicio/covid.php?pais=${country}`
+
+    const promise = axios.get<Record<number, IInfo>>(URL)
+
+    promise
+      .then(({ data }) => {
+        const infos = Object.values(data)
+
+        setCountry(infos[0].Pais)
+        const totalConfirmed = infos.reduce(
+          (acc, curr) => curr.Confirmados + acc,
+          0
+        )
+        const totalDead = infos.reduce((acc, curr) => curr.Mortos + acc, 0)
+
+        setConfirmed(totalConfirmed)
+        setDead(totalDead)
+
+        setInfo(infos)
+      })
+      .catch(() => alert('Ocorreu um na API de Covid\nTente mais tarde'))
+      .finally(() => setLoading(false))
+  }
+
+  return { fetch, country, dead, confirmed, info, date }
+}
