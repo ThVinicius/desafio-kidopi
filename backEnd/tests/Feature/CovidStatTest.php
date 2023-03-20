03d0607bbd8a1d8343160df2cbf5fc8d099ba2ba
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-03-20 16:30:57 -0300

    remove refresh database

--- backEnd/tests/Feature/CovidStatTest.php
+++ backEnd/tests/Feature/CovidStatTest.php
@@ -2,13 +2,10 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CovidStatTest extends TestCase
 {
-  use RefreshDatabase;
 
   public function testSaveCovidStat()
   {
